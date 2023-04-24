12fc5750073514d17dccecb55f0096bb4d3dbd2a
Dzhem Salimov <djkomgames@gmail.com>
2023-04-24 09:47:35 +0300

    Add files via upload

---
+++ GameLib/src/Suit.java
@@ -0,0 +1,18 @@
+
+public enum Suit {
+    CLUBS("CLUBS"),
+    DIAMONDS("DIAMONDS"),
+    HEARTS("HEARTS"),
+    SPADES("SPADES");
+
+    private final String suit;
+
+    Suit(String suit) {
+        this.suit = suit;
+    }
+
+    String getSuit() {
+        return this.suit;
+    }
+
+}
