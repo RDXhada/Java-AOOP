65585e3478fe76579630a256df83cd9bd07d49ee
Dzhem Salimov <djkomgames@gmail.com>
2023-06-16 20:53:10 +0300

    Delete potgotovkaJavaKontr1 directory

--- potgotovkaJavaKontr1/src/AnimalInstance.java
+++
@@ -1,9 +0,0 @@
-public class AnimalInstance {
-    private final String INSTANCE_ID;
-    private static int counter = 1;
-
-    public AnimalInstance(String instanceId) {
-        INSTANCE_ID = instanceId;
-    }
-
-}
